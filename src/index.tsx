e38f8f3b61699cb2d22c16c0b27c807c0d065af7
Taranjeet Singh <taranjeet.singh@telekom-digital.com>
2022-01-07 13:41:43 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -53,10 +53,6 @@ export const Provider = (props: {children: React.ReactNode}) => {
   useEffect(() => {
     // initialise the store
     const s = {};
-    const ls = {
-      red: '{"page":2,"per_page":6,"total":12,"total_pages":2,"data":[{"id":7,"email":"michael.lawson@reqres.in","first_name":"Michael","last_name":"Lawson","avatar":"https://reqres.in/img/faces/7-image.jpg"},{"id":8,"email":"lindsay.ferguson@reqres.in","first_name":"Lindsay","last_name":"Ferguson","avatar":"https://reqres.in/img/faces/8-image.jpg"},{"id":9,"email":"tobias.funke@reqres.in","first_name":"Tobias","last_name":"Funke","avatar":"https://reqres.in/img/faces/9-image.jpg"},{"id":10,"email":"byron.fields@reqres.in","first_name":"Byron","last_name":"Fields","avatar":"https://reqres.in/img/faces/10-image.jpg"},{"id":11,"email":"george.edwards@reqres.in","first_name":"George","last_name":"Edwards","avatar":"https://reqres.in/img/faces/11-image.jpg"},{"id":12,"email":"rachel.howell@reqres.in","first_name":"Rachel","last_name":"Howell","avatar":"https://reqres.in/img/faces/12-image.jpg"}],"support":{"url":"https://reqres.in/#support-heading","text":"To keep ReqRes free, contributions towards server costs are appreciated!"}}'
-    };
-
 
     if(localStorage && localStorage.length) {
       for ( var i: any = 0, len = localStorage.length; i < len; ++i ) {
@@ -70,21 +66,10 @@ export const Provider = (props: {children: React.ReactNode}) => {
             
           } catch (error) {
             console.error(`Problem with save-data storage KEY:${key}`);
-            
           }
-
         }
       }
     }
-
-
-    for(let prop in ls) {
-      if(ls.hasOwnProperty(prop)) {
-        const key = prop.replace(`${preName}-`, "");
-        s[key] = JSON.parse(ls[prop]);
-      }
-    }
-
     setStore(s);
   }, []);
 
